42e590db2e01e64bc2329af10fb7373c4e5f0cb6
Grijesh S <144785218+garry8877@users.noreply.github.com>
2024-11-15 17:45:26 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,1 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CustomERC20Token is ERC20, Ownable(msg.sender) {
+
+    string public TokenName = "MetaToken";
+    string public TokenSymbol = "MTN";
+
+    constructor() ERC20(TokenName,TokenSymbol) {
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to,amount);
+    }
+
+    function burn(address to, uint256 amount) public {
+        _burn(to,amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+}
